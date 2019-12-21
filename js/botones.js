b9ba3e5bd7d0e3a486946cd7f5c0d3ab207b8039
Leonardo Lopez <leodanlors1993@gmail.com>
2019-12-21 13:31:10 -0600

    dentAV

---
+++ js/botones.js
@@ -0,0 +1,23 @@
+let llamar = () => {
+    location.href = 'tel:5535705892'
+    console.log('llamaste')
+}
+let whatsapp = () => {
+    
+    console.log('whatsappeaste')
+    return gtag_report_conversion('https://api.whatsapp.com/send?phone=525561161513')
+}
+
+let polanco = () => {
+    location.href = 'https://goo.gl/maps/EYqkp5yuy3miKNBw7'
+    console.log('fuiste a planco')
+}
+
+let fresno = () => {
+    location.href = 'https://goo.gl/maps/JFMnNrVBpf2GzREK9'
+    console.log('fuiste a fresno')
+}
+let dentarios = () => {
+    location.href = 'https://www.facebook.com/dentarioscdmx/'
+    console.log('fuiste a dntarios fb')
+}
